0976bd03ed1836a126968ad59f7a960cedb6a89e
denis <dmurinka@gmail.com>
2020-10-14 21:49:28 +0300

    edited data` insert value

--- app.py
+++ app.py
@@ -63,6 +63,13 @@ class Blockchain:
         self.block.next = block
         self.block = self.block.next
 
+    # def valid_check(self):
+    #     last_block = []
+    #     if self.block.previous_hash != self.block.hash:
+    #         return  False
+    #     if not
+
+
     def mine(self, block):
         for n in range(self.maxNonce):
             if int(block.hash(), 16) <= self.target:
@@ -71,8 +78,9 @@ class Blockchain:
 
                 session.add(
                     Blocks(
-                        blockNo=str(block.blockNo),
+
                         data=str(block.data),
+                        blockNo=str(block.blockNo),
                         hash = str( block.hash()) ,
                         prevHash=str(block.previous_hash).encode('utf-8'),
                         timestamp=block.timestamp
@@ -86,9 +94,10 @@ class Blockchain:
 blockchain = Blockchain()
 
 for n in range(10):
-    blockchain.mine(Block(Block("Block " + str(n+1)))) #select * from blocks where  blocks.id between 5 and 95
-    #blockchain.mine(Block("Block " + str((session.query(Blocks.blockNo).order_by(Blocks.id.desc()).first())[0])))
-    #blockchain.mine(Block("Block " + str(n+1)))
+
+    #blockchain.mine(Block(Block("Block " + str((session.query(Blocks.blockNo).order_by(Blocks.id.desc()).first())[0]))))
+    #blockchain.mine(Block(Block("some data")))
+    blockchain.mine(Block("some data"))
 
 while blockchain.head != None:
     #print(blockchain.head)
