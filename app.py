cce011f92b27dad6bf4f476a067cf27727e52b87
denis <dmurinka@gmail.com>
2020-10-25 21:44:16 +0200

    little adjustments of flask

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ import hashlib
 from blocks_database import Blocks, engine, sessionmaker
 import logging
 
+from sqlalchemy.exc import OperationalError, ProgrammingError
+
+from flask import Flask, jsonify, request
+from uuid import uuid4
+
+from sqlalchemy import column
+
 Session = sessionmaker(bind=engine)
 session = Session()
 
@@ -10,8 +17,13 @@ logging.basicConfig(filename='sqlalchemyLogging.log',
                     filemode='w',
                     format='%(name)s - %(levelname)s - %(message)s')
 
-log = logging.getLogger("ex")
+log = logging.getLogger("ERROR --------> ")
 
+app = Flask(__name__)
+
+
+# Генерируем уникальный на глобальном уровне адрес для этого узла
+node_identifier = str(uuid4()).replace('-', '')
 
 class Block:
     # c = session.query(Blocks).order_by(Blocks.id.desc()).limit(2)     #hot ot get last one row in db
@@ -32,8 +44,7 @@ class Block:
 
     def __init__(self, data):
         self.data = data
-        # self.hashh = self.hash()
-        # print("HASHH  "+str(self.hashh))
+
 
 
     def hash(self):
@@ -50,13 +61,20 @@ class Block:
         return h.hexdigest()
 
 
-    def __str__(self):
 
+    def __str__(self):
+        block = {
+            'index': str(self.blockNo),
+            'timestamp': str(self.timestamp),
+            'transactions':  str(self.data).encode('utf-8'),
+            'previous_hash': str(self.previous_hash).encode('utf-8'),
+        }
         return "Block Hash: " + str(self.hash()) + \
                "\nBlockNo: " + str(self.blockNo) + \
                "\nBlock Data: " + str(self.data) + \
                "\nHashes: " + str(self.nonce) + \
                "\n--------------"
+       # return jsonify(block), 200
 
 
 class Blockchain:
@@ -67,7 +85,7 @@ class Blockchain:
     target = 2 ** (256 - diff)
 
     block = Block("Genesis")
-    dummy = head = block
+   # dummy = head = block
 
     def add(self, block):
 
@@ -77,37 +95,35 @@ class Blockchain:
         self.block.next = block
         self.block = self.block.next
 
-
-
+    # def __str__(self):
+    #     return self
     # def valid_check(self):
-    #
-    #     #if self.block.previous_hash != self.block.hash:
-    #       #  return  False
-    #     if self.block.hash == self.block.hash():
-    #         print("False")
+
 
 
     def mine(self, block):
         for n in range(self.maxNonce):
             if int(block.hash(), 16) <= self.target:
                 self.add(block)
-                #print("Prev Hash:" + str(block.previous_hash))
-                print(block)
 
+                print(block)
+                #print(str(column('id') == 967 ))
 
                 session.add(
                     Blocks(
 
                         data=str(block.data),
                         blockNo=str(block.blockNo),
-                        hash = str( block.hash()) ,
+                        hash=str(block.hash()),
                         prevHash=str(block.previous_hash).encode('utf-8'),
                         timestamp=block.timestamp
                     )
                 )
                 session.commit()
 
-                #logging.exception("Exception occurred")
+
+
+                logging.exception("Exception occurred")
                 log.exception(block)
                 break
             else:
@@ -115,19 +131,33 @@ class Blockchain:
 
 blockchain = Blockchain()
 
+@app.route('/mine', methods=['GET'])
+def mine():#http://127.0.0.0/mine
+
+
+    return "We'll mine a new Block"
+
+
 
 if __name__ == '__main__':
 
     try :
-        for n in range(10):
+        #app.run()
 
-            #blockchain.mine(Block(Block("Block " + str((session.query(Blocks.blockNo).order_by(Blocks.id.desc()).first())[0]))))
-            #blockchain.mine(Block(Block("some data")))
+        for n in range(10):
+        #     # blockchain.mine(Block(Block("Block " + str((session.query(Blocks.blockNo).order_by(Blocks.id.desc()).first())[0]))))
+        #     # blockchain.mine(Block(Block("some data")))
             blockchain.mine(Block("some data"))
 
-        while blockchain.head != None:
-            #print(blockchain.head)
-            blockchain.head = blockchain.head.next
+        # while blockchain.head != None:
+        #     blockchain.head = blockchain.head.next
+
+
+
+    except (ProgrammingError, OperationalError) as e:
+        logging.exception("Exception occurred")
+        log.exception(e)
+
     except Exception as e:
         logging.exception("Exception occurred")
         #log.exception(e)
