596aa6658efb29346e107864f3d0115a920a6f58
denis <dmurinka@gmail.com>
2020-11-28 09:43:00 +0200

    flask work preparation

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import hashlib
 import json
-from textwrap import dedent
+
+from urllib.parse import urlparse
+
+import requests
 from time import time
 from uuid import uuid4
 
-from flask import Flask
+from flask import Flask, jsonify, request
 
 class Blockchain(object):
+    def __init__(self):
+        ...
+        self.nodes = set()
+        ...
+
+    def register_node(self, address):
+        """
+        Вносим новый узел в список узлов
+
+        :param address: <str> адрес узла , другими словами: 'http://192.168.0.5:5000'
+        :return: None
+        """
+
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
     ...
 
     def proof_of_work(self, last_proof):
@@ -39,7 +57,68 @@ class Blockchain(object):
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
 
+    def valid_chain(self, chain):
+        """
+        Проверяем, является ли внесенный в блок хеш корректным
 
+        :param chain: <list> blockchain
+        :return: <bool> True если она действительна, False, если нет
+        """
+
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+            # Проверьте правильность хеша блока
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # Проверяем, является ли подтверждение работы корректным
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts(self):
+        """
+        Это наш алгоритм Консенсуса, он разрешает конфликты,
+        заменяя нашу цепь на самую длинную в цепи
+
+        :return: <bool> True, если бы наша цепь была заменена, False, если нет.
+        """
+
+        neighbours = self.nodes
+        new_chain = None
+
+        # Ищем только цепи, длиннее нашей
+        max_length = len(self.chain)
+
+        # Захватываем и проверяем все цепи из всех узлов сети
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # Проверяем, является ли длина самой длинной, а цепь - валидной
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        # Заменяем нашу цепь, если найдем другую валидную и более длинную
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
 # Создаем экземпляр узла
 app = Flask(__name__)
 
@@ -52,13 +131,47 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return "We'll mine a new Block"
+    # Мы запускаем алгоритм подтверждения работы, чтобы получить следующее подтверждение…
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # Мы должны получить вознаграждение за найденное подтверждение
+    # Отправитель “0” означает, что узел заработал крипто-монету
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    # Создаем новый блок, путем внесения его в цепь
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
 
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    return "We'll add a new transaction"
+    values = request.get_json()
 
+    # Убедитесь в том, что необходимые поля находятся среди POST-данных
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Создание новой транзакции
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
@@ -68,6 +181,41 @@ def full_chain():
     }
     return jsonify(response), 200
 
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
